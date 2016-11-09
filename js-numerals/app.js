b19cad294d399f669a7be038028ef109893807e7
melkotoury <melkotoury@gmail.com>
2016-11-09 20:15:31 +0200

    fix :  resolve input  accepting strings  :bug:

--- js-numerals/app.js
+++ js-numerals/app.js
@@ -4,6 +4,16 @@
 
 (function() {
 
+    // Make Sure input is only number
+
+    document.getElementById("num").onkeypress = function (evt) {
+        evt = (evt) ? evt : window.event;
+        var charCode = (evt.which) ? evt.which : evt.keyCode;
+        if (!(charCode > 31 && (charCode < 48 || charCode > 57))) {
+            return true;
+        }
+        return false;
+    };
 
     const arr = x => Array.from(x);
     const num = x => Number(x) || 0;
