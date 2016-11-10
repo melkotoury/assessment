d18d98635d75a87b5362fa09142a795b3a0775ec
melkotoury <melkotoury@gmail.com>
2016-11-10 21:34:19 +0200

    gh-pages update

---
+++ js-numerals/app/app.js
@@ -0,0 +1,79 @@
+'use strict';
+
+/**
+ * Created by Mahmoud El Kotoury on 11/9/2016.
+ */
+
+(function () {
+
+    // Make Sure input is only number
+
+    document.getElementById("num").onkeypress = function (evt) {
+        evt = evt ? evt : window.event;
+        var charCode = evt.which ? evt.which : evt.keyCode;
+        if (!(charCode > 31 && (charCode < 48 || charCode > 57))) {
+            return true;
+        }
+        return false;
+    };
+
+    function convert(n) {
+        var NUMERALS = [{ value: 1000000000000000000000, str: "sextillion" }, { value: 1000000000000000000, str: "quintillion" }, { value: 1000000000000000, str: "quadrillion" }, { value: 1000000000000, str: "trillion" }, { value: 1000000000, str: "billion" }, { value: 1000000, str: "million" }, { value: 1000, str: "thousand" }, { value: 100, str: "hundred" }, { value: 90, str: "ninety" }, { value: 80, str: "eighty" }, { value: 70, str: "seventy" }, { value: 60, str: "sixty" }, { value: 50, str: "fifty" }, { value: 40, str: "forty" }, { value: 30, str: "thirty" }, { value: 20, str: "twenty" }, { value: 19, str: "nineteen" }, { value: 18, str: "eighteen" }, { value: 17, str: "seventeen" }, { value: 16, str: "sixteen" }, { value: 15, str: "fifteen" }, { value: 14, str: "fourteen" }, { value: 13, str: "thirteen" }, { value: 12, str: "twelve" }, { value: 11, str: "eleven" }, { value: 10, str: "ten" }, { value: 9, str: "nine" }, { value: 8, str: "eight" }, { value: 7, str: "seven" }, { value: 6, str: "six" }, { value: 5, str: "five" }, { value: 4, str: "four" }, { value: 3, str: "three" }, { value: 2, str: "two" }, { value: 1, str: "one" }];
+
+        if (n < 0) {
+            return "minus " + convert(-n);
+        } else if (n === 0) {
+            return "zero";
+        } else {
+            var result = "";
+            var _iteratorNormalCompletion = true;
+            var _didIteratorError = false;
+            var _iteratorError = undefined;
+
+            try {
+                for (var _iterator = NUMERALS[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+                    var numeral = _step.value;
+
+                    if (n >= numeral.value) {
+
+                        if (n < 100) {
+                            result += numeral.str;
+                            n -= numeral.value;
+                            if (n > 0) result += "-";
+                        } else {
+                            var times = Math.floor(n / numeral.value);
+                            if (times == 1) {} else {
+
+                                result += convert(times) + " " + numeral.str;
+                                n -= numeral.value * times;
+                                if (n > 0) result += " and ";
+                            }
+                        }
+                    }
+                }
+            } catch (err) {
+                _didIteratorError = true;
+                _iteratorError = err;
+            } finally {
+                try {
+                    if (!_iteratorNormalCompletion && _iterator.return) {
+                        _iterator.return();
+                    }
+                } finally {
+                    if (_didIteratorError) {
+                        throw _iteratorError;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+
+    var bConvert = document.getElementById("bConvert");
+    bConvert.onclick = function () {
+        var num = document.getElementById("num").value;
+        var display = document.getElementById("display");
+        display.value = convert(num);
+    };
+})();
