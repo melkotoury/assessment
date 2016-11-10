89379910ce584e74380a247787db26bc43c449a0
melkotoury <melkotoury@gmail.com>
2016-11-10 09:59:00 +0200

    fix :  resolve 'and ' in the output text phrase  :bug:

--- js-numerals/app.js
+++ js-numerals/app.js
@@ -1,91 +1,91 @@
+'use strict';
+
+
 /**
  * Created by Mahmoud El Kotoury on 11/9/2016.
  */
 
-(function() {
-
-
-
+(function () {
 
     // Make Sure input is only number
 
     document.getElementById("num").onkeypress = function (evt) {
-        evt = (evt) ? evt : window.event;
-        var charCode = (evt.which) ? evt.which : evt.keyCode;
+        evt = evt ? evt : window.event;
+        var charCode = evt.which ? evt.which : evt.keyCode;
         if (!(charCode > 31 && (charCode < 48 || charCode > 57))) {
             return true;
         }
         return false;
     };
 
-    const arr = x => Array.from(x);
-    const num = x => Number(x) || 0;
-    const isEmpty = xs => xs.length === 0;
-    const take = n => xs => xs.slice(0,n);
-    const drop = n => xs => xs.slice(n);
-    const reverse = xs => xs.slice(0).reverse();
-    const comp = f => g => x => f (g (x));
-    const not = x => !x;
-    const chunk = n => xs =>
-    isEmpty(xs) ? [] : [take(n)(xs), ...chunk (n) (drop (n) (xs))];
-
-
-
-// numToWords :: (Number a, String a) => a -> String
-    let numToWords = n => {
-
-        let a = [
-            '', 'one', 'two', 'three', 'four',
-            'five', 'six', 'seven', 'eight', 'nine',
-            'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
-            'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+    function convert(n) {
+        let NUMERALS = [
+            {value: 1000000000000000000000, str: "sextillion"},
+            {value: 1000000000000000000, str: "quintillion"},
+            {value: 1000000000000000, str: "quadrillion"},
+            {value: 1000000000000, str: "trillion"},
+            {value: 1000000000, str: "billion"},
+            {value: 1000000, str: "million"},
+            {value: 1000, str: "thousand"},
+            {value: 100, str: "hundred"},
+            {value: 90, str: "ninety"},
+            {value: 80, str: "eighty"},
+            {value: 70, str: "seventy"},
+            {value: 60, str: "sixty"},
+            {value: 50, str: "fifty"},
+            {value: 40, str: "forty"},
+            {value: 30, str: "thirty"},
+            {value: 20, str: "twenty"},
+            {value: 19, str: "nineteen"},
+            {value: 18, str: "eighteen"},
+            {value: 17, str: "seventeen"},
+            {value: 16, str: "sixteen"},
+            {value: 15, str: "fifteen"},
+            {value: 14, str: "fourteen"},
+            {value: 13, str: "thirteen"},
+            {value: 12, str: "twelve"},
+            {value: 11, str: "eleven"},
+            {value: 10, str: "ten"},
+            {value: 9, str: "nine"},
+            {value: 8, str: "eight"},
+            {value: 7, str: "seven"},
+            {value: 6, str: "six"},
+            {value: 5, str: "five"},
+            {value: 4, str: "four"},
+            {value: 3, str: "three"},
+            {value: 2, str: "two"},
+            {value: 1, str: "one"}
         ];
 
-        let b = [
-            '', '', 'twenty', 'thirty', 'forty',
-            'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
-        ];
+        if (n < 0) {
+            return "minus " + convert(-n);
+        } else if (n === 0) {
+            return "zero";
+        } else {
+            let result = "";
+            for (let numeral of NUMERALS) {
+                if (n >= numeral.value) {
+                    if (n < 100) {
+                        result += numeral.str;
+                        n -= numeral.value;
+                        if (n > 0) result += "-";
+                    } else {
+                        let times = Math.floor(n / numeral.value);
+                        result += convert(times) + " " + numeral.str;
+                        n -= numeral.value * times;
+                        if (n > 0) result += " and ";
+                    }
+                }
+            }
+            return result;
+        }
+    }
 
-        let g = [
-            '', 'thousand', 'million', 'billion', 'trillion', 'quadrillion',
-            'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion'
-        ];
-
-
-        let makeGroup = ([ones,tens,huns]) => {
-            return [
-                num(huns) === 0 ? '' : a[huns] + ' hundred ',
-                num(ones) === 0 ? b[tens] : b[tens] && b[tens] + '-' || '',
-                a[tens+ones] || a[ones]
-            ].join('');
-        };
-
-        let thousand = (group,i) => group === '' ? group : `${group} ${g[i]}`;
-
-        if (typeof n === 'number')
-            return numToWords(String(n));
-        else if (n === '0')
-            return 'zero';
-        else
-            return comp (chunk(3)) (reverse) (arr(n))
-                .map(makeGroup)
-                .map(thousand)
-                .filter(comp(not)(isEmpty))
-                .reverse()
-                .join(' ');
+    var bConvert = document.getElementById("bConvert");
+    bConvert.onclick = function () {
+        var num = document.getElementById("num").value;
+        console.log(convert(num));
+        var display = document.getElementById("display");
+        display.value = convert(num);
     };
-
-    let bConvert = document.getElementById("bConvert");
-    bConvert.onclick = function  () {
-        let num = document.getElementById("num").value;
-        console.log(numToWords(num));
-        let display = document.getElementById("display");
-        display.value = numToWords(num);
-
-    };
-
-
-
-})
-
-();
+})();
